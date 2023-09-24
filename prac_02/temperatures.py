281eecb43fcbd74c8c16144b963694e80290a485
Quinn-Stocker <quinn.stocker@my.jcu.edu.au>
2023-09-25 01:23:59 +1000

    Completed Temperatures prac

---
+++ prac_02/temperatures.py
@@ -0,0 +1,37 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+
+def fahrenheit_calc(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+def celsius_calc(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+    
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_calc(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_calc(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+main()
