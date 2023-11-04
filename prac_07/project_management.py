948a420d647cb8bd41d094c4523573453864be62
Quinn-Stocker <quinn.stocker@my.jcu.edu.au>
2023-11-04 16:26:22 +1000

    Completed task

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -81,29 +81,49 @@ def main():
     print("Thank you for using custom-built project management software.")
 
 
+"""Define the load_file function, a function used to load info from a file"""
+
+
 def load_file(file_name):
     projects = []
+    """Open the File in Read mode"""
     with open(file_name, "r") as in_file:
+        # Read the first line in the file, the headers, to ignore them in the list
         in_file.readline()
+        # loop through all lines in file after headers
         for line in in_file:
+            # Split line into list for processing
             line = line.split("\t")
+            # Process list
             line[-1] = (line[-1]).strip()
             start_date = datetime.strptime(line[1], '%d/%m/%Y').date()
+            # Convert List into an Object
             project_info = Project(line[0], start_date, line[2], line[3], int(line[4]))
+            # Append the new project Object to the projects list
             projects.append(project_info)
     return projects
 
 
+"""Define the save_file function, a function used to save the list to a file"""
+
+
 def save_file(file_name, projects):
+    """Open the File in Write mode"""
     with open(file_name, "w") as in_file:
+        # Write header line
         in_file.write("Name	Start Date	Priority	Cost Estimate	Completion Percentage\n")
+        # Write all Objects in projects list
         for project in projects:
             in_file.write(project.write_string())
+
+
+"""Define the get_file_name function, a function used get a file name"""
 
 
 def get_file_name():
     wrong_file_name = True
     file_name = ""
+    # Loop until Input has no error
     while wrong_file_name:
         file_name = input("File Name: ")
         if file_name != FILENAME:
@@ -113,12 +133,16 @@ def get_file_name():
     return file_name
 
 
+"""Define the display_projects function"""
+
+
 def display_projects(projects):
+    # Display the incomplete projects
     print("Incomplete projects:")
     for project in projects:
         if not project.is_completed():
             print(project)
-
+    # Display the completed projects
     print("\nCompleted projects:")
     for project in projects:
         if project.is_completed():
@@ -126,11 +150,17 @@ def display_projects(projects):
     print("\n")
 
 
+"""Define the update_projects function, used to update either completion percentage or priority"""
+
+
 def update_project(projects):
+    # Print all projects with a number value linking to it
     for project_num, project in enumerate(projects):
         print(f"{project_num} - {project}")
+    # Get a project choice and print it
     project_choice = int(get_num_float_input("Project choice: ", False))
     print(projects[project_choice])
+    # Get percentage and priority values, if empty don't change
     percentage = get_num_float_input("New Percentage: ", True)
     priority = get_num_float_input("New Priority: ", True)
     project_info = projects[project_choice]
@@ -141,12 +171,21 @@ def update_project(projects):
     projects[project_choice] = project_info
 
 
+"""Define the filter_data function, used to filter the data and display all projects occurring after given date"""
+
+
 def filter_data(projects):
+    # get a start date for filtering
     date = get_date_input("Show projects that start after date (dd/mm/yy): ")
+    # Sort the data
     projects.sort()
+    # loop through all projects only printing when the start date is greater than or equal to the given start date
     for project in projects:
         if project.start_date >= date:
             print(project)
+
+
+"""Define the get_name_input function, used to get a name input with error checking"""
 
 
 def get_name_input(input_text):
@@ -159,6 +198,9 @@ def get_name_input(input_text):
         else:
             error = False
     return user_input
+
+
+"""Define the get_date_input function, used to get a date input with error checking"""
 
 
 def get_date_input(input_text):
@@ -175,6 +217,9 @@ def get_date_input(input_text):
         except ValueError:
             print("Invalid input; enter a valid number")
     return date_input
+
+
+"""Define the get_num_float_input function, used to get either a int or float input with error checking"""
 
 
 def get_num_float_input(input_text, allow_null):
